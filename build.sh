07132f82d94120056abb1073a93769a1c9ae97e4
myxi <myxi@envs.net>
2024-03-24 12:24:43 +0530

    fix: include dlls in windows release

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p bin/windows
+mkdir -p bin/windows/dlls
+mkdir -p bin/linux
+
+nim c -d:ssl -d:release --opt:size --cpu:amd64 \
+    -o:bin/linux/povi-amd64-linux.bin src/povi
+nim c -d:ssl -d:mingw -d:release --opt:size --cpu:amd64 \
+    -o:bin/windows/povi-amd64-windows.exe src/povi
+
+cd bin/windows
+wget https://nim-lang.org/download/dlls.zip
+unzip dlls.zip -d bin/windows/dlls
+
+zip povi-amd64-windows.zip \
+    povi-amd64-windows.exe \
+    dlls/cacert.pem \
+    dlls/libcrypto-1_1-x64.dll \
+    dlls/libssl-1_1-x64.dll \
+    dlls/pcre64.dll
